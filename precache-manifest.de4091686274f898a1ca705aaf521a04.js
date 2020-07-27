2863ac69922fdb5b9efb7aa8915bccc46a1ac3eb
soringherghisan <sorin.gherghisan@gmail.com>
2020-07-28 00:01:36 +0300

    Updates

---
+++ precache-manifest.de4091686274f898a1ca705aaf521a04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022b18aa0d692862007cea3aa821803e",
+    "url": "/sports_timer_v5/index.html"
+  },
+  {
+    "revision": "c50bbd1b2d02ddfbfae1",
+    "url": "/sports_timer_v5/static/css/main.efdb5423.chunk.css"
+  },
+  {
+    "revision": "44ed4ed192aa73ade8ca",
+    "url": "/sports_timer_v5/static/js/2.acefb4d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sports_timer_v5/static/js/2.acefb4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50bbd1b2d02ddfbfae1",
+    "url": "/sports_timer_v5/static/js/main.a1c2e27f.chunk.js"
+  },
+  {
+    "revision": "a66f466cbddd648affca",
+    "url": "/sports_timer_v5/static/js/runtime-main.0c041099.js"
+  },
+  {
+    "revision": "7b196d77b7b92dda2ccd31bb47e37643",
+    "url": "/sports_timer_v5/static/media/countdownBegins.7b196d77.wav"
+  },
+  {
+    "revision": "d02396a3a1beb648e07d5833129fbbad",
+    "url": "/sports_timer_v5/static/media/finish.d02396a3.wav"
+  },
+  {
+    "revision": "6d539868a91447633339d8dd2b891f1f",
+    "url": "/sports_timer_v5/static/media/lastSecondBeep.6d539868.wav"
+  },
+  {
+    "revision": "475b3522ab7b32db5b21e2566ba6e783",
+    "url": "/sports_timer_v5/static/media/lastSecondsBeep.475b3522.wav"
+  }
+]);
